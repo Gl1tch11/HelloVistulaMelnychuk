bff2a87d588e130696254231d91348605d13e0d7
gl1tch1 <omelnyc3@stu.vistula.edu.pl>
2022-10-14 14:58:07 +0200

    Second commit

--- src/pl/vistula/helloworldnazwisko/HelloWorldNazwisko.java
+++ src/pl/vistula/helloworldnazwisko/HelloWorldNazwisko.java
@@ -3,5 +3,6 @@ package pl.vistula.helloworldnazwisko;
 public class HelloWorldNazwisko {
     public static void main(String[] args) {
         System.out.println("hello World");
+        System.out.println("hello World2");
     }
 }
